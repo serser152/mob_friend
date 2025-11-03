8f02a9a6063ae34eae8ecb767879791c6cfb277a
serser152 <serser152@yandex.ru>
2025-11-03 18:41:23 +0300

    added base dialog ui

--- UI/app_ui.py
+++ UI/app_ui.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 import streamlit as st
+from time import sleep
+import sys
+import os
+subdir = os.getcwd()
+sys.path.append(subdir)
+from agent.agent import ask_llm
+st.write(subdir)
 
 st.header('hello')
+st.markdown(' ** applicatoion assistant ** ')
+
+promt = st.text_input('Message:')
+with st.spinner("Wait for it...", show_time=True):
+    ans = ask_llm(promt)
+st.write(ans)
